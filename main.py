0c6bc2769eef6ac5069b1ecc5fa1c82936741a70
Python <python@ZiMac4-3.local>
2019-11-13 21:39:29 +0100

    examples_flasks

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from persistence import update_from_front
+
+app = Flask(__name__)
+
+@app.route('/users', methods = ['POST'])
+def users():
+    if request.method == 'POST':
+        data = request.json
+        update_from_front(data)
+
+if __name__ == "__main__":
+    app.run()
